a5ec0ea56185853e7ea0935e404b9328f7ac90c4
coder <tsering@Tsering>
2022-11-26 10:27:20 +0545

    Navigation Complete with Saas

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        {/* LEFT MENU */}
+        <div className="left">
+          <div className="item">
+            <img src="img/eng.jpeg" alt="eng" width="25" height="25" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">Children</Link>
+          </div>
+        </div>
 
-export default Navbar
+        {/* CENTER MENU */}
+        <div className="center">
+        <Link className="link" to="/">DEVSTORE</Link>
+        </div>
+  
+        {/* RIGHT MENU */}
+
+        <div className="right">
+        <div className="item">
+            <Link className="link" to="/">Home</Link>
+          </div>
+        <div className="item">
+            <Link className="link" to="/">About</Link>
+          </div>
+        <div className="item">
+            <Link className="link" to="/">Store</Link>
+          </div>
+        <div className="item">
+            <Link className="link" to="/">Contact</Link>
+          </div>
+
+          <div className="icons">
+            <SearchIcon/>
+            <PersonOutlineIcon/>
+            <FavoriteBorderIcon/>
+         <div className="cartIcon">
+         <ShoppingCartIcon/>
+         <span>0</span>
+         </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
