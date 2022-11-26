e871ed3c871445139ab52c92712fa1e55a91511a
coder <tsering@Tsering>
2022-11-26 12:46:50 +0545

    Footer Section Complete

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='top'>
+          <div className='item'>
+            <h1>Categories</h1>
+            <span>Men</span>
+            <span>Women</span>
+            <span>Shoes</span>
+            <span>Accessories</span>
+            <span>New Arrivals</span>
+          </div>
+          <div className='item'>
+          <h1>Links</h1>
+            <span>FAQ</span>
+            <span>Pages</span>
+            <span>Store</span>
+            <span>Policy</span>
+            <span>Cookies</span>
+          </div>
+          <div className='item'>
+          <h1>About</h1>
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.t</span>
+          </div>
+          <div className='item'>
+            <h1>Contact</h1>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+          </div>
+      </div>
+
+
+      <div className='bottom'>
+        <div className='left'>
+          <span className='logo'>Devstore</span>
+          <span className='copyrights'>  © Copyrights 2023. All rights reserved </span>
+        </div>
+        <div className='right'>
+          <img src='img/payment.png' alt='payment'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
